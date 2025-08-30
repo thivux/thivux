8d6ff02298917ae4dc8cb75da515c8690978616a
thivux <70306448+thivux@users.noreply.github.com>
2025-08-30 10:10:47 +0000

    Deploying to gh-pages from @ thivux/thivux.github.io@3bf823721cc8a50fd21499288f2a844a2995ed46 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "thivux/thivux",
+    "data-repo": "thivux/thivux.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
